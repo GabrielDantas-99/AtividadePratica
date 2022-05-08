bb197ec216f380ed3102016024ecbabaf2f99f5c
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-05-08 17:48:46 -0300

    Camada de Serviço e Registro de Componentes

--- src/main/java/com/esigsoftware/atividade/resources/TaskResource.java
+++ src/main/java/com/esigsoftware/atividade/resources/TaskResource.java
@@ -1,19 +1,33 @@
 package com.esigsoftware.atividade.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.esigsoftware.atividade.entities.Task;
+import com.esigsoftware.atividade.services.TaskService;
 
 @RestController
 @RequestMapping(value = "/tasks")
 public class TaskResource {
 
+	@Autowired
+	private TaskService service;
+	
 	@GetMapping
-	public ResponseEntity<Task> findAll() {
-		Task t = new Task(1L, "Criar", "Criar Interface", "Gabriel", "Alta", "22/05/2022");
-		return ResponseEntity.ok().body(t);
+	public ResponseEntity<List<Task>> findAll() {
+		List<Task> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Task> findById(@PathVariable Long id){
+		Task obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/esigsoftware/atividade/services/TaskService.java
@@ -0,0 +1,26 @@
+package com.esigsoftware.atividade.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.esigsoftware.atividade.entities.Task;
+import com.esigsoftware.atividade.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+
+	@Autowired
+	private TaskRepository repository;
+	
+	public List<Task> findAll(){
+		return repository.findAll();
+	}
+	
+	public Task findById(Long id) {
+		Optional<Task> obj = repository.findById(id);
+		return obj.get();
+	}
+}
